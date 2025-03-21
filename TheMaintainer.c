2e6de951db30a36894c39a21753ce33ce828e94c
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-21 04:15:09 +0100

    Update TheMaintainer.c

--- TheMaintainer.c
+++ TheMaintainer.c
@@ -10,8 +10,6 @@ struct TheMailConditioner*GetTheMaintainer(u8*value){
     return GetTheMailConditioner((u8[]){value[0],value[1],value[2],value[3],value[4],value[5],value[6],value[7],value[8],value[9],value[10],value[11],value[12],value[13],value[14],value[15],value[16]},17,true);
 }
 EXPORT_SYMBOL(GetTheMaintainer);
-
-
 static void Layer1End(void){}
 static void Layer0Start(void){}
 Layer0_1Setup("TheMaintainer",0,0)
