175a026165551781662f4cab2a235de1d6e6f341
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-22 10:58:23 +0000

    Update on 2025-03-22 10:58:23

--- TheMaintainer.h
+++ TheMaintainer.h
@@ -4,35 +4,35 @@
 extern struct TheMailConditioner*GetTheMaintainer(u8*value);
 #define TM(name, ...) \
     struct TheMailConditione*tmc##name=GetTheMaintainer((u8[]){__VA_ARGS__}); \
-    struct name*name##API=NULL; \
+    struct name*name##TM=NULL; \
     if(!GetTheMailConditionerData(tmc##name)) { \
-        name##API=(struct name*)GetTheMailConditionerData(GetTheMailConditionerUnsafeData(GetTheMaintainer((u8[]){__VA_ARGS__}))); \
-        printk(KERN_ERR #name ": Can't get the " #name " API.\n"); \
+        name##TM=(struct name*)GetTheMailConditionerData(GetTheMailConditionerUnsafeData(GetTheMaintainer((u8[]){__VA_ARGS__}))); \
+        printk(KERN_ERR #name ": Can't get the " #name " TM.\n"); \
     }
 #define SetupTM(description,version,build,...) \
-    static struct TheMailConditione*tmcTMAPI; \
+    static struct TheMailConditione*tmcTM; \
     static void End(void){ \
         TMEnd(); \
-        CancelTheMailConditioner(tmcTMAPI); \
+        CancelTheMailConditioner(tmcTM); \
     } \
     static void AutoDeleteTM(void* data,struct ExpiryWorkBaseBenchmark){ \
         kfree(data); \
     } \
     static void Start(void){ \
-        tmcTMAPI=GetTheMaintainer((u8[]){__VA_ARGS__}); \
-        if (!GetTheMailConditionerUnsafeData(tmcTMAPI)){ \
+        tmcTM=GetTheMaintainer((u8[]){__VA_ARGS__}); \
+        if (!GetTheMailConditionerUnsafeData(tmcTM)){ \
             void*tmObject=kmalloc(sizeof(struct description),GFP_KERNEL); \
             if(!tmObject){ \
-                printk(KERN_ERR #description ": Can't allocate memory for the " #description " API.\n"); \
+                printk(KERN_ERR #description ": Can't allocate memory for the " #description " TM.\n"); \
                 End(); \
                 return; \
             } \
-            if(!SetTheMailConditionerData(tmcTMAPI,tmObject)){ \
-                printk(KERN_ERR #description ": Can't set the " #description " API data.\n"); \
+            if(!SetTheMailConditionerData(tmcTM,tmObject)){ \
+                printk(KERN_ERR #description ": Can't set the " #description " TM data.\n"); \
                 End(); \
                 return; \
             } \
-            SetAutoDeleteTheMailConditioner(tmcTMAPI,AutoDeleteTM); \
+            SetAutoDeleteTheMailConditioner(tmcTM,AutoDeleteTM); \
             TMStart(); \
         } \
     } \
