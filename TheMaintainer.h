d4f5e15c95576918454afd10f89f21ad36737d5f
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-24 09:46:00 +0000

    Update on 2025-03-24 09:46:00

--- TheMaintainer.h
+++ TheMaintainer.h
@@ -18,23 +18,21 @@ extern struct TheMailConditioner*GetTheMaintainer(u8*value);
     static void AutoDeleteTM(void* data,struct ExpiryWorkBaseBenchmark){ \
         if(!TMEndCalled)\
             TMEndCalled=true; \
-        if(data) { \
+        if(data)\
             TMEnd(data); \
-            if(data) \
+        if(data) \
             kfree(data); \
-            data=NULL;\
-        } \
+        data=NULL;\  
     } \
     static void End(void){ \
         if(TMEndCalled) \
             return; \
         void*data=GetTheMailConditionerData(tmcTM); \
-        if(data) { \
+        if(data)\
             TMEnd(data); \
-            if(data) \
+        if(data) \
             kfree(data); \
-            data=NULL;\
-        } \
+        data=NULL;\
     } \
     static void Start(void){ \
         u8 TMKey[17]={__VA_ARGS__}; \
@@ -49,7 +47,7 @@ extern struct TheMailConditioner*GetTheMaintainer(u8*value);
             return; \
         } \
         if (!GetTheMailConditionerData(tmcTM)){ \
-            void*tmObject=kmalloc(sizeof(struct description),GFP_KERNEL); \
+            struct description*tmObject=kmalloc(sizeof(struct description),GFP_KERNEL); \
             if(!tmObject){ \
                 printk(KERN_ERR #description ": Can't allocate memory for the " #description " TM.\n"); \
                 End(); \
@@ -60,98 +58,12 @@ extern struct TheMailConditioner*GetTheMaintainer(u8*value);
                 End(); \
                 return; \
             } \
-            SetAutoDeleteTheMailConditioner(tmcTM,AutoDeleteTM); \
+            SetupExpiryWorkBase(&tmObject->ewb,GetTheMailConditionerEWB(tmcTM), tmObject, AutoDeleteTM); \  
             TMStart(); \
         } \
     } \
     Setup(description,version,build)
 #define SetTM(name,method)name->method=method
-#define SetupGetAddTMInData() \
-        struct TMList*owner;  \
-        struct list_head tm
-#define SetupGetAddTMInStruct(name) \
-        struct name*(*Get)(u8*value);\
-        struct name*(*Add)(u8*value); \
-        SetupGetAddTMInData()
-#define SetupGetAddTMInMainTMStart(object) \
-        INIT_LIST_HEAD(&object->tm); \
-        SetTM(object,Add); \
-        SetTM(object,Get)
-#define SetupGetAddTMInCode(name,object,size,add) \
-        static DEFINE_MUTEX(GetAddTMMutex);\
-        struct TMList { \
-            struct list_head list; \
-            u8 value[size]; \
-        }; \
-        static struct object *Get(u8*value); \
-        static struct object *Get(u8*value){ \
-            Get##name##TM(); \
-            struct TMList *entry = NULL, *match = NULL; \
-            list_for_each_entry(entry, &name##TM->tm, list) \
-                if (memcmp(entry->value, value, size) == 0) { \
-                    match = entry; \
-                    break; \
-                } \
-            if (!match) return NULL; \
-            struct TheMailConditioner*tmcTM=GetTheMailConditioner(match->value, size, false); \
-            if (!tmcTM) return NULL; \
-            return (struct object*)GetTheMailConditionerData(tmcTM); \
-        } \
-        static void AutoDeleteTMData(void*data,struct ExpiryWorkBaseBenchmark); \
-        static struct object*Add(u8*value);\
-        static struct object*Add(u8*value){\
-            struct object*data=Get(value);\
-            if(data)return data;\
-            mutex_loc(&GetAddTMMutex);\
-            data=Get(value);\
-            if(data){\
-                mutex_unlock(&GetAddTMMutex);\
-                return data;\
-            }\
-            struct TheMailConditioner*_tmcTM=GetTheMailConditioner(value,6,true);\
-            if(!_tmcTM){\
-                mutex_unlock(&GetAddTMMutex);\
-                return NULL;\
-            }\
-            data=kmalloc(sizeof(struct object),GFP_KERNEL);\
-            if(!SetTheMailConditionerData(_tmcTM,data)){\
-                mutex_unlock(&GetAddTMMutex); \
-                return NULL; \
-            } \
-            struct TMList*tmList=kmalloc(sizeof(struct TMList),GFP_KERNEL);\
-            if(!tmList){\
-                kfree(data);\
-                mutex_unlock(&GetAddTMMutex);\
-                return NULL;\
-            }\
-            memcpy(tmList->value, value, size);\
-            INIT_LIST_HEAD(&tmList->list);\
-            Get##name##TM();\
-            list_add(&tmList->list, &name##TM->tm); \
-            SetupExpiryWorkBase(&data->ewb,&name##TM->ewb,data,AutoDeleteTMData); \
-            data->owner=tmList;\
-            add \
-            mutex_unlock(&GetAddTMMutex);\
-            return data;\
-        } \
-        static void AutoDeleteTMData(void*vdata,struct ExpiryWorkBaseBenchmark){ \
-            if(!vdata)return; \
-            mutex_loc(&GetAddTMMutex);\
-            struct name *data=(struct object*)vdata; \
-            Get##name##TM();\
-            list_del(&data->owner->list);\
-            mutex_unlock(&GetAddTMMutex);\
-            kfree(data->owner);\
-            if(!vdata)\
-            kfree(vdata); \
-        } 
-#define SetupGetAddTMInMainTMEnd(object)\
-        struct TMList *entry, *tmp; \
-        list_for_each_entry_safe(entry, tmp,&object->tm,list){ \
-            struct TheMailConditioner*tmc=GetTheMailConditioner(entry->value,6,false);\
-            if (tmc)\
-                CancelTheMailConditioner(tmc);\
-        }
-    
+
 #endif
 
